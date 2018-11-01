9deff9a8a86dbc03ab2e7972bab48811c4ffdaaa
stivens73 <stivens54@gmail.com>
2018-10-31 19:42:42 -0700

    minor fixes

--- dao/mysql/migrations/3_transactions.sql
+++ dao/mysql/migrations/3_transactions.sql
@@ -1,10 +1,10 @@
 -- +migrate Up
 CREATE TABLE `transactions` (
-  `tx_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `tx_id` int(10) UNSIGNED NOT NULL,
   `mer_pubkey` varchar(128) NOT NULL,
   `mer_asset_code` varchar(12) NOT NULL,
   `amount` int(10) UNSIGNED NOT NULL,
-  `secret` text NOT NULL,
+  `secret` varchar(60) NOT NULL,
   `tx_created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -17,4 +17,4 @@ ALTER TABLE `transactions`
   MODIFY `tx_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 -- +migrate Down
-DROP TABLE transactions
+DROP TABLE transactions;
